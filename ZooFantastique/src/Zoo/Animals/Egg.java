f989dab6300c8ff9a96815d5c87c771917dd20c1
Camille Girodengo <115540569+Giro4c@users.noreply.github.com>
2023-11-10 08:34:21 +0100

    Restructuration project

---
+++ ZooFantastique/src/Zoo/Animals/Egg.java
@@ -0,0 +1,40 @@
+package Zoo.Animals;
+
+public class Egg {
+
+	private Creature newBorn;
+	private final int incubationTime;
+	private int incubationProgress;
+	
+	
+	
+	public Egg(String specie, String name, boolean isMale, int weight, int height, int age, String indicatorHunger, boolean isSleeping,
+			String indicatorHealth, int incubationTime, int incubationProgress) {
+		this.newBorn = new Creature(specie, name, isMale, weight, height, age, indicatorHunger, isSleeping, indicatorHealth);
+		this.incubationTime = incubationTime;
+		this.incubationProgress = incubationProgress;
+	}
+
+	public int getIncubationTime() {
+		return incubationTime;
+	}
+
+	public int getIncubationProgress() {
+		return incubationProgress;
+	}
+
+	public void setIncubationProgress(int incubationProgress) {
+		this.incubationProgress = incubationProgress;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Egg [incubationTime=" + incubationTime + ", incubationProgress=" + incubationProgress + "]";
+	}
+
+	public Creature hatch() {
+		
+	}
+	
+}
---
+++ ZooFantastique/src/Zoo/Animals/Nymphe.java
@@ -0,0 +1,16 @@
+package Zoo.Animals;
+
+import Zoo.Caracteristics.Revivable;
+
+public class Nymphe extends Mammal implements Revivable {
+
+	public Nymphe(String specie, String name, boolean isMale, int weight, int height, int age, String indicatorHunger,
+			boolean isSleeping, String indicatorHealth, int gestationTime, int gestationProgress) {
+		super(specie, name, isMale, weight, height, age, indicatorHunger, isSleeping, indicatorHealth, gestationTime,
+				gestationProgress);
+		// TODO Auto-generated constructor stub
+	}
+
+		
+	
+}
