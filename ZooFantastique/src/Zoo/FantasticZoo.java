5337658871f7f1f5b175865119cd844bbd5f7891
Camille Girodengo <115540569+Giro4c@users.noreply.github.com>
2023-11-16 14:49:23 +0100

    Print choices on terminal with GameEngine

--- ZooFantastique/src/Zoo/FantasticZoo.java
+++ ZooFantastique/src/Zoo/FantasticZoo.java
@@ -5,7 +5,10 @@ import java.util.ArrayList;
 public class FantasticZoo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		GameEngine game = new GameEngine();
+//		while (true) {
+			game.showChoices();
+//		}
 
 	}
 	
--- ZooFantastique/src/Zoo/GameEngine.java
+++ ZooFantastique/src/Zoo/GameEngine.java
@@ -1,5 +1,33 @@
 package Zoo;
 
+import Zoo.Prompts.Prompt;
+
 public class GameEngine {
 
+	private Prompt promptGenerator;
+	private FantasticZoo zoo;
+
+	public GameEngine() {
+		super();
+		this.promptGenerator = new Prompt(1);
+		this.zoo = new FantasticZoo("Zoo", new ZooMaster("Camille", false, 19), 3);
+	}
+
+	public Prompt getPromptGenerator() {
+		return promptGenerator;
+	}
+	
+	public FantasticZoo getZoo() {
+		return zoo;
+	}
+
+	public void showChoices() {
+		if (this.promptGenerator.getSituationIndicator() < 3) {
+			System.out.println("What do you want to do ?");
+			for (String action : this.promptGenerator.getFullPrompt(zoo, null)) {
+				System.out.println('\t' + action);
+			}
+		}
+	}
+	
 }
