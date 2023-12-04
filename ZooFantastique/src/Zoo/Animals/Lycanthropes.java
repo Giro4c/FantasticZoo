90a4cc416bc3db3c8da6a89fb74328076f600bb4
Robin <robin.lerouge@etu.univ-amu.fr>
2023-12-04 21:53:35 +0100

    changement constructeur add enclosure

--- ZooFantastique/src/Zoo/Animals/Lycanthropes.java
+++ ZooFantastique/src/Zoo/Animals/Lycanthropes.java
@@ -1,13 +1,14 @@
 package Zoo.Animals;
 
+import Zoo.Enclosure;
 import Zoo.Caracteristics.CanRun;
 
 public class Lycanthropes extends Mammal implements CanRun {
 
 	public Lycanthropes(String specie, String name, boolean isMale, int weight, int height, int age,
-			String indicatorHunger, boolean isSleeping, String indicatorHealth, int gestationTime, int gestationProgress) {
+			String indicatorHunger, boolean isSleeping, String indicatorHealth, int gestationTime, int gestationProgress, Enclosure enclosure) {
 		super(specie, name, isMale, weight, height, age, indicatorHunger, isSleeping, indicatorHealth, gestationTime,
-				gestationProgress);
+				gestationProgress, enclosure);
 		// TODO Auto-generated constructor stub
 	}
 }
--- ZooFantastique/src/Zoo/Animals/Megalodon.java
+++ ZooFantastique/src/Zoo/Animals/Megalodon.java
@@ -1,12 +1,13 @@
 package Zoo.Animals;
 
+import Zoo.Enclosure;
 import Zoo.Caracteristics.CanSwim;
 
 public class Megalodon extends Oviparous implements CanSwim {
 
 	public Megalodon(String specie, String name, boolean isMale, int weight, int height, int age, String indicatorHunger,
-			boolean isSleeping, String indicatorHealth) {
-		super(specie, name, isMale, weight, height, age, indicatorHunger, isSleeping, indicatorHealth);
+			boolean isSleeping, String indicatorHealth, Enclosure enclosure) {
+		super(specie, name, isMale, weight, height, age, indicatorHunger, isSleeping, indicatorHealth, enclosure);
 		// TODO Auto-generated constructor stub
 	}
 
