69cca6f2f7594d74f26d322355956731ab2f89c1
moundir213 <bcho0407@gmail.com>
2023-12-05 22:36:43 +0100

    pushSave

--- WerewolfColony/src/Colony/mainTest.java
+++ WerewolfColony/src/Colony/mainTest.java
@@ -9,9 +9,9 @@ public class mainTest {
 	
 		// test constituteCoupleAlpha
         Werewolf alphaMale = new Werewolf("Wolf", "AlphaMale", true, 50, 120, 3, "Young", false, 100, 'α', 5, 8, null);
-        Werewolf female1 = new Werewolf("Wolf", "Female1", false, 40, 100, 2, "Young", false, 85, 'ψ', 4, 7, null);
-        Werewolf female2 = new Werewolf("Wolf", "Female2", false, 45, 110, 2, "Young", false, 80, 'γ', 4, 4, null);
-        Territory territory = new Territory();
+        Werewolf female1 = new Werewolf("Wolf", "Female1", false, 44, 100, 2, "Young", false, 81, 'δ', 4, 8, null);
+        Werewolf female2 = new Werewolf("Wolf", "Female2", false, 45, 110, 3, "Young", false, 81, 'γ', 5, 8, null);
+      /*  Territory territory = new Territory();
         Pack packtest = new Pack(territory);
         packtest.getMembers().add(alphaMale);
         packtest.getMembers().add(female1);
@@ -19,10 +19,15 @@ public class mainTest {
         packtest.setTerritory(territory);
         packtest.constituteAlphaCouple(alphaMale);
         System.out.println(packtest.getAlphaCouple().getFemale().getRank());
-        System.out.println(packtest.getAlphaCouple().getPack());
+        System.out.println(packtest.getAlphaCouple().getPack()); **/
        
         
-        System.out.println(female2.canDominate(female1));
+        System.out.println(female1.canDominate(female2));
+        System.out.println(female1.getStrength());
+        System.out.println(female1.getRank());
+        System.out.println(female2.getStrength());
+        System.out.println(female2.getRank());
+
         
         
 	}
